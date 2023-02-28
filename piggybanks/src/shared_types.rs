0609f03e83f955325807a6206d977048faccca35
rster2002 <rster2002dev@gmail.com>
2023-02-28 17:58:32 +0100

    💡 Expand the comment for the db_transaction macro a bit

--- piggybanks/src/shared_types.rs
+++ piggybanks/src/shared_types.rs
@@ -8,7 +8,9 @@ pub type SharedPool = State<DbPool>;
 pub type SharedJwtService = State<JwtService>;
 pub type DbTransaction<'a> = sqlx::Transaction<'a, Postgres>;
 
-/// Used to create the impl argument type for code that needs an executor
+/// Used to create the impl argument type for code that needs an executor. A macro is used here as
+/// type aliases that contain the impl keyword are unstable as explained
+/// [here](https://github.com/rust-lang/rust/issues/63063).
 #[macro_export]
 macro_rules! db_executor {
     ($lt:lifetime) => {
