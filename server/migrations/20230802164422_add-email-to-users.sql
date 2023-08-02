87181d4a8bf74b8a53218f3c8e27d06f14c698a4
Bjorn <rster2002dev@gmail.com>
2023-08-02 18:46:58 +0200

    🗃️ Add email to users table

---
+++ server/migrations/20230802164422_add-email-to-users.sql
@@ -0,0 +1,2 @@
+ALTER TABLE users
+ADD COLUMN email varchar(255) not null default 'default@example.com';
--- server/src/models/dto/auth/register_user_dto.rs
+++ server/src/models/dto/auth/register_user_dto.rs
@@ -1,7 +1,9 @@
 use serde::Deserialize;
+use serde_email::Email;
 
 #[derive(Debug, Deserialize)]
 pub struct RegisterUserDto<'a> {
     pub username: &'a str,
+    pub email: Email,
     pub password: &'a str,
 }
